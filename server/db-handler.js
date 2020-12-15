7f752c1d7d47d9daf5e00abf80142f5877e2bdb9
Joshua Pelealu <45566099+JoshuaPelealu@users.noreply.github.com>
2020-12-14 21:31:34 -0500

    Initialized Firebase

--- server/db-handler.js
+++ server/db-handler.js
@@ -1,8 +1,10 @@
 const firebase = require('firebase/app');
 const admin = require('firebase-admin');
-const serviceAccount = require('./database/high-thrills-firebase-adminsdk.json');
+const serviceAccount = require('./firebase/high-thrills-firebase-adminsdk.json');
 // Add Firebase products individually
 require('firebase/auth');
+require('firebase/firestore');
+process.env.FIRESTORE_EMULATOR_HOST = 'localhost:8080';
 
 const firebaseConfig = {
 	apiKey            : 'AIzaSyAw-zFeGnwfSiET2gZrZaVZebnZyajeR4Q',
@@ -11,11 +13,30 @@ const firebaseConfig = {
 	storageBucket     : 'high-thrills.appspot.com',
 	messagingSenderId : '365192472687',
 	appId             : '1:365192472687:web:cea4c3c565845ce5ae459a',
-	measurementId     : 'G-2DJXH2BFMT',
-	credential        : admin.credential.applicationDefault(),
-	databaseURL       : 'https://high-thrills.firebaseio.com'
+	measurementId     : 'G-2DJXH2BFMT'
 };
 
-firebase.initializeApp(firebaseConfig);
+admin.initializeApp({
+	credential  : admin.credential.cert(serviceAccount)
+});
 
-module.exports = function dbConnection(app) {};
+// firebase.initializeApp();
+
+const db = admin.firestore();
+if (process.argv.slice(2).indexOf('emulator') > -1) 
+  // db.useEmulator("localhost", 8080)
+
+module.exports = function dbConnection(app) {
+  app.post('/login', async (req, res) => {
+    console.log(req.body, db);
+    const userRef = db.collection('users');
+    const user = await userRef.get();
+    user.forEach(doc => {
+      console.log(doc.id, '=> ', doc.data());
+      res.send(doc.data())
+    })
+
+
+
+  })
+};
